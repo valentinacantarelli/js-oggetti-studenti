9265135a7c2da52e6952c935e460f7ef37b06aef
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-07 16:24:51 +0200

    aggiunto ciclo for stampa studenti

--- js/script.js
+++ js/script.js
@@ -12,7 +12,41 @@ var studente={
 };
 
 //ciclo for in
-
 for(var key in studente){
     console.log(key);
 }
+
+//array studenti
+var studenti=[
+    {"nome":"Oliver",
+    "cognome":"Walker",
+    "età":15
+    },
+    {"nome":"Robert",
+    "cognome":"Robinson",
+    "età":72
+    },
+    {"nome":"Agatha",
+    "cognome":"Davies",
+    "età":27
+    },
+    {"nome":"Calista",
+    "cognome":"Williams",
+    "età":36
+    },
+    {"nome":"Dotty",
+    "cognome":"Wilson",
+    "età":21
+    },
+
+];
+studenti.push(studente);
+console.log(studenti);
+
+//ciclo per stampare nome e cognome studente
+var lista=document.getElementById("studenti")
+ for(var i=0; i<studenti.length;i++){
+    lista.innerHTML+=`<li>Nome studente : ${studenti[i]["nome"]} ${studenti[i]["cognome"]}</li>`
+ }
+
+ //
